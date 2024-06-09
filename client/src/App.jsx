3f78f39fc23e9636dd5b848910b5484e9114f571
anurag-2420 <anuragpatel20june@gmail.com>
2024-06-09 19:02:38 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Header from "./components/header"
+import Footer from "./components/footer"
+import Home from "./pages/Home"
+function App() {
+  return (
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles/header.css'
+const header = () => {
+  return (
+    <div>
+      footer
+    </div>
+  )
+}
+
+export default header
