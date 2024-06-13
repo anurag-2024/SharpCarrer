d257a8109feac56a2faab68c7a13a6b92e028d79
PRIYANSHU KUMAR RAJ <priyanshuraj2552@gmail.com>
2024-06-13 20:06:15 +0530

    added userControllers and routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
+import userRouter from './routes/userRoutes.js'
 
 dotenv.config(); // load environment variable
 connectDB(); // connect to mongoDB
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors()); // Enable cors
 
 // Routes
+app.use('/api/user',userRouter  );
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
