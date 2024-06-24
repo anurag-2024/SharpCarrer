8c6cc142fbc3d4cda09152c94c02f858ea719a91
anurag-2420 <anuragpatel20june@gmail.com>
2024-06-24 14:31:00 +0530

    Marquee added

--- client/src/components/Destinations.jsx
+++ client/src/components/Destinations.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './styles/Destinations.css';
 import { topDestinations, internationalStays } from '../data/data'; // Adjust the path as necessary
-
+import Marquee from 'react-fast-marquee';
 const Destinations = () => {
   return (
     <>
       <div className="dest_d_container">
         <h1 className='dest_d_title'>𝚃𝚘𝚙 𝙳𝚎𝚜𝚝𝚒𝚗𝚊𝚝𝚒𝚘𝚗𝚜</h1>
         <div className="dest_upper_cont">
+          <Marquee gradient={false} speed={40} pauseOnClick pauseOnHover loop={0}>
           {topDestinations.map(destination => (
             <div className="dest_box" key={destination.id}>
               <img src={destination.image} alt={`Destination ${destination.name}`} />
               <h1>{destination.name}</h1>
             </div>
           ))}
+          </Marquee>
         </div>
         <div className="dest_low_cont">
           <div className="dest_texts">
@@ -21,12 +23,14 @@ const Destinations = () => {
             <h3 className='dest_right'>𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐭𝐢𝐨𝐧𝐚𝐥 𝐒𝐭𝐚𝐲𝐬</h3>
           </div>
           <div className="dest_items">
+          <Marquee gradient={false} speed={40} pauseOnClick pauseOnHover loop={0}>
             {internationalStays.map(stay => (
               <div className="dest_item" key={stay.id}>
                 <img src={stay.image} alt={`Destination ${stay.name}`} />
                 <h3>{stay.name}</h3>
               </div>
             ))}
+          </Marquee>
           </div>
         </div>
       </div>
