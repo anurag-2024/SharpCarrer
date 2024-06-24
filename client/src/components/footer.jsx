aa64e8dfdbfc3d9e94bb976bcd74231338a9b395
anurag-2420 <anuragpatel20june@gmail.com>
2024-06-24 14:50:22 +0530

    footer fixed

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./styles/footer.css";
 import { FaMapMarkerAlt, FaTwitter, FaYoutube, FaFacebook, FaInstagram } from 'react-icons/fa';
-import location from '../assets/images/location.png'
+import logo from '../assets/logo.png'
+import { Link } from "react-router-dom";
 const Footer = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -16,22 +17,21 @@ const Footer = () => {
       [name]: value
     });
   };
-
+  const handleClick = () => {
+    window.scrollTo(0, 0, 'smooth');
+  }
   return (
     <>
       <div className="contact_container">
         <div className="contact_left">
           <div className="contact_location">
-           <img src={location} alt="" />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448196.5263208281!2d76.76357549901144!3d28.643684629087268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1719219807534!5m2!1sen!2sin" width={450} height={370} style={{border:0}} allowfullscreen={""}></iframe>
           </div>
           <div className="contact_left_text">
             <div className="contact_logo">
-               <img src={location} alt="" />
+               <Link to="/"><img src={logo} alt="" onClick={()=>handleClick()}/></Link>
             </div>
             <div className="texts">
-                <div className="header">
-                    <h1>SharpSkill</h1>
-                </div>
                 <div className="social_media">
                     <a href=""><FaTwitter size={30} /></a>
                     <a href=""><FaYoutube size={30} /></a>
