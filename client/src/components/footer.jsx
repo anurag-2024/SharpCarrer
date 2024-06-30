f41cc843fe569a327d8dbe6a1e999c1eba31a3d5
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-06-30 23:07:38 +0300

    improved footer for mobile

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -28,11 +28,11 @@ const Footer = () => {
       <div className="bg-dark py-5">
         <Container>
           <Row>
-            <Col lg={6}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448196.5263208281!2d76.76357549901144!3d28.643684629087268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1719219807534!5m2!1sen!2sin" className="w-100 h-100" style={{border:0}} allowfullscreen={""}></iframe>
+            <Col lg={6} className="mb-3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448196.5263208281!2d76.76357549901144!3d28.643684629087268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1719219807534!5m2!1sen!2sin" className="w-100 h-lg-100" style={{border:0}} allowfullscreen={""}></iframe>
             </Col>
-            <Col lg={6}>
-              <Form>
+            <Col lg={6} className="mb-3">
+              <Form className="bg-secondary p-3">
                 <Form.Group className="mb-3">
                   <Form.Control
                    type="text"
