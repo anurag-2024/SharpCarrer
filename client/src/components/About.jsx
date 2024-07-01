9eb63d88c7904081202f1c75f896358b00a64737
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-07-01 13:11:01 +0300

    improved about section for small and medium screen

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,6 +8,7 @@ import staff from "../assets/images/staff.png";
 import client from "../assets/images/client.png";
 import room from "../assets/images/room.png";
 import { Row, Col, Container } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
 
 const About = () => {
   return (
@@ -15,9 +16,9 @@ const About = () => {
       <div className="mt-5 py-5">
         <Container className="py-5">
           <Row className="align-items-stretch">
-            <Col lg={4}>
+            <Col lg={4} className="mb-5">
               <Row className="align-items-stretch h-100">
-                <Col md={6} className="d-flex flex-column justify-content-between">
+                <Col xs={6} className="d-flex flex-column justify-content-between">
                   <div className="ps-3 border-start border-2 border-secondary mb-3" style={{ height: '40%' }}>
                     <img src={img1} alt="Image 1" className="img-fluid h-100 object-fits-cover" />
                   </div>
@@ -25,7 +26,7 @@ const About = () => {
                     <img src={img4} alt="Image 4" className="img-fluid h-100" />
                   </div>
                 </Col>
-                <Col md={6} className="d-flex flex-column justify-content-between">
+                <Col xs={6} className="d-flex flex-column justify-content-between">
                   <div className="pt-3 border-top border-2 border-secondary ms-3 mb-3" style={{ height: '40%' }}>
                     <img src={img3} alt="Image 3" className="img-fluid h-100 object-fits-cover" />
                   </div>
@@ -58,83 +59,45 @@ const About = () => {
           </Row>
         </Container>
       </div>
-      {/*<div className="about_about_container mt-5 py-3">
-        <div className="about_upper_container">
-          <div className="about_img_box">
-            <div className="about_img1_box">
-              <div className="about_img1_line"></div>
-              <div className="about_img1">
-                <img src={img1} alt="Image 1" />
+      <Container fluid className="bg-primary">
+        <Container className="py-5">
+          <Row className="text-light">
+            <Col md={4} className="px-3">
+              <div className="d-flex bg-dark py-4 mx-lg-5 mb-3 mt-md-5">
+                <div className="w-50 text-center">
+                  <Icon.PeopleFill className="h1" />
+                </div>
+                <div className="w-50 text-center">
+                  <div className="data">Staff</div>
+                  <div className="data">2345</div>
+                </div>
               </div>
-            </div>
-            <div className="about_img2_box">
-              <div className="about_img2_line"></div>
-              <div className="about_img2">
-                <img src={img2} alt="Image 2" />
+            </Col>
+            <Col md={4} className="px-3">
+              <div className="d-flex bg-dark py-4 mb-3 mx-lg-5 mb-md-5">
+                <div className="w-50 text-center">
+                  <Icon.PersonLinesFill className="h1" />
+                </div>
+                <div className="w-50 text-center">
+                  <div className="data">Clients</div>
+                  <div className="data">2345</div>
+                </div>
               </div>
-            </div>
-            <div className="about_img3_box">
-              <div className="about_img3">
-                <img src={img3} alt="Image 3" />
+            </Col>
+            <Col md={4} className="px-3">
+              <div className="d-flex bg-dark py-4 mx-lg-5 mb-3 mt-md-5">
+                <div className="w-50 text-center">
+                  <Icon.BuildingFill className="h1" />
+                </div>
+                <div className="w-50 text-center">
+                  <div className="data">Rooms</div>
+                  <div className="data">2345</div>
+                </div>
               </div>
-              <div className="about_img3_line"></div>
-            </div>
-            <div className="about_img4_box">
-              <div className="about_img4">
-                <img src={img4} alt="Image 4" />
-              </div>
-              <div className="about_img4_line"></div>
-            </div>
-          </div>
-          <div className="about_text_box">
-            <div className="about_about_heading">
-              <hr className="about_separator" />
-              <h2>About Us</h2>
-              <hr className="about_separator" />
-            </div>
-            <h1>Welcome To SharpSkill</h1>
-            <h3>Your Gateway To Exceptional Stays</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              officiis, est vero reprehenderit provident laborum similique, iusto
-              repellat quis ipsam fugit ullam! Repudiandae, qui accusamus sunt
-              praesentium voluptas natus earum nemo odit veritatis enim. Expedita,
-              id aperiam porro molestiae assumenda minima ullam dolore rerum ipsum
-              recusandae exercitationem, adipisci hic doloremque! Magni blanditiis
-              repellendus omnis natus hic earum consequuntur asperiores deleniti.
-            </p>
-          </div>
-        </div>
-        <div className="about_lower_container">
-          <div className="about_box_lower1">
-            <div className="about_img">
-              <img src={staff} alt="Staff" />
-            </div>
-            <div className="about_texts">
-              <h1 className="data">Staff</h1>
-              <h1 className="data">2345</h1>
-            </div>
-          </div>
-          <div className="about_box_lower2">
-            <div className="about_img">
-              <img src={client} alt="Client" />
-            </div>
-            <div className="about_texts">
-              <h1 className="data">Client</h1>
-              <h1 className="data">2345</h1>
-            </div>
-          </div>
-          <div className="about_box_lower3">
-            <div className="about_img">
-              <img src={room} alt="Room" />
-            </div>
-            <div className="about_texts">
-              <h1 className="data">Room</h1>
-              <h1 className="data">2345</h1>
-            </div>
-          </div>
-        </div>
-      </div>*/}
+            </Col>
+          </Row>
+        </Container>
+      </Container>
     </>
   );
 };
