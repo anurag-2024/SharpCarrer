3ec9429f244d6cc449660c29940c91d0f16a4cdb
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-07-02 14:55:33 +0300

    updated bootstrap variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { UserProvider } from './context/UserContext.jsx';
-import './index.css'
+import './index.scss'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
